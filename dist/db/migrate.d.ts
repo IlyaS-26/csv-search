9e0079329a902b245b48b66da31ab2e88ad19aea
Ilya Sergeev <ilyasergeev2626@gmail.com>
2025-10-18 19:56:49 +0300

    Add upload .csv file method

---
+++ dist/db/migrate.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Проведение инициализации SQL-таблицы перед началом работы сервиса
+ */
+export declare function runMigrations(): Promise<void>;
+//# sourceMappingURL=migrate.d.ts.map
---
+++ dist/service/index.js
@@ -0,0 +1,23 @@
+import fastify from "fastify";
+import multipart from "@fastify/multipart";
+import { pipeline } from "node:stream/promises";
+import { createWriteStream } from "node:fs";
+import path from "node:path";
+const app = fastify({
+    logger: true
+});
+await app.register(multipart);
+app.post("/uploadCSV", async (request, reply) => {
+    const file = await request.file();
+    if (!file)
+        return reply.code(400).send({ error: "No file" });
+    const dir = path.join("src/service/uploads");
+    const destination = path.join(dir, "data.csv");
+    await pipeline(file.file, createWriteStream(destination));
+    return reply.code(201).send({ mimetype: file.mimetype });
+});
+await app.listen({
+    port: 3000,
+    host: "0.0.0.0"
+});
+//# sourceMappingURL=index.js.map
