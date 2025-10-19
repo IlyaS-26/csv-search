f43e4436da7bdce2c5e70b486a9adb4c50a95894
Ilya Sergeev <ilyasergeev2626@gmail.com>
2025-10-19 19:04:22 +0300

    small changes

--- src/service/index.ts
+++ src/service/index.ts
@@ -3,6 +3,7 @@ import multipart from "@fastify/multipart";
 import { pipeline } from "node:stream/promises";
 import { createWriteStream } from "node:fs";
 import path from "node:path";
+import "dotenv/config";
 
 const app = fastify({
     logger: true
@@ -10,18 +11,24 @@ const app = fastify({
 
 await app.register(multipart);
 
-app.post("/uploadCSV", async (request, reply) => {
+app.post("/upload", async (request, reply) => {
     const file = await request.file();
-    if (!file) return reply.code(400).send({ error: "No file" });
+
+    if (!file) {
+        return reply.code(400).send({ error: "No file" });
+    }
+    if (file.filename.split(".")[1] !== "csv") {
+        return reply.code(400).send({ error: "Not a .csv file" });
+    }
 
     const dir = path.join("src/service/uploads");
     const destination = path.join(dir, "data.csv");
 
     await pipeline(file.file, createWriteStream(destination));
-    return reply.code(201).send({ mimetype: file.mimetype });
+    return reply.code(201).send("File successfully upload");
 });
 
 await app.listen({
-    port: 3000,
-    host: "0.0.0.0"
+    port: Number(process.env.PORT) || 3000,
+    host: process.env.HOST || "0.0.0.0"
 });
